8ed476d35dea09369aa8e8d963cf777ba45a4bfa
Lukasz Lucko <lukaszl95@o2.pl>
2017-12-28 12:12:41 +0100

    zadanie-9.5

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # zadanie-9.5
+https://lukaszl95.github.io/zadanie-9.5/
---
+++ script.js
@@ -0,0 +1,5 @@
+var buttons = document.getElementsByClassName('button');
+for (var i = 0; i <buttons.length; i++) {
+	var buttonsText = buttons[i].innerText;
+	console.log(buttonsText);
+}
